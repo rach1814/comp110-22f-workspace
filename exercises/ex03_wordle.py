b7d74809ae2ea4c388cc78456e5eb385d206138c
rachel lopez <rach1814@email.unc.edu>
2022-09-14 18:17:15 -0400

    final exercise 3 code

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,35 +1,35 @@
 """ex03 Structured Wordle."""
 __author__ = "730334012"
 
-from time import strftime
 
-
-def contains_char(ss: str, sc: str) -> bool: # ss is the string that is being searched through, sc is the single character that we are looking for
-    """when given two strings, will search through the first for single character (second string) and will return true if character is found."""
+def contains_char(ss: str, sc: str) -> bool:  # ss is the string that is being searched through, sc is the single character that we are looking for
+    """When given two strings, will search through the first for single character (second string) and will return true if character is found."""
     assert len(sc) == 1
-    i: int = 0 # index tracker
+    i: int = 0  # index tracker
     while i < len(ss):
         if ss[i] == sc:
             return True
         i = i + 1
     return False
 
+
 # white, green and yellow emojis
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+
 def emojified(guess: str, secret: str) -> str:
-    """When given two strings of equal length (a guess and a secret), it will return colored emojis based on matching characters in the guess and the secret"""
-    assert len(guess) == len(secret) #the guess must be the same length as the secret
-    i: int = 0 # index tracker
+    """When given two strings of equal length (a guess and a secret), it will return colored emojis based on matching characters in the guess and the secret."""
+    assert len(guess) == len(secret)  # the guess must be the same length as the secret
+    i: int = 0  # index tracker
     es: str = ""
 
     while i < len(guess):
         if guess[i] == secret[i]:
             es += GREEN_BOX
             i = i + 1
-        elif contains_char(secret, guess[i]) == True:
+        elif contains_char(secret, guess[i]) is True:
             es += YELLOW_BOX
             i = i + 1
         else:
@@ -37,17 +37,17 @@ def emojified(guess: str, secret: str) -> str:
             i = i + 1
     return es
 
+
 def input_guess(expected_length: int) -> str:
-    """when given an expected length of a guess, will prompt the user for a guess until they guess a word of the expected length """
+    """When given an expected length of a guess, will prompt the user for a guess until they guess a word of the expected length."""
     user_guess: str = input(f"Enter a {expected_length} character word: ")
     while len(user_guess) != expected_length:
         user_guess: str = input(f"That wasn't {expected_length} chars! Try again: ")
     return user_guess
 
+
 def main() -> None:
-    """The entrypoint of the program and main game loop """ #establish what secret word is as a variable, keep track of how many turns user has spent, whether user has won game, & control flow of game.
-    i: int = 0 # index tracker
-    es: str = ""
+    """The entrypoint of the program and main game loop."""  # establish what secret word is as a variable, keep track of how many turns user has spent, whether user has won game, & control flow of game.
     secret: str = "codes"
     expected_length: int = len(secret)
     user_guess: str = ""
@@ -63,5 +63,6 @@ def main() -> None:
             print("X/6 - Sorry, try again tomorrow!")
         turns = turns + 1
     
+
 if __name__ == "__main__":
     main()
